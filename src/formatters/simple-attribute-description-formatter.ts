80e7c131e2b1ec636a8c206a200f563c3a3fa2be
Wesley Thomas <wthomas3@kent.edu>
2021-04-24 14:29:50 -0400

    updated the simple formatters to produce descriptions similar to those generated by VS Code using JavaDocs.

--- src/formatters/simple-attribute-description-formatter.ts
+++ src/formatters/simple-attribute-description-formatter.ts
@@ -2,11 +2,37 @@ import * as ts from 'typescript';
 import { AttributeDescriptionFormatter } from './attribute-description-formatter';
 
 /**
- * Creates an attribute description that simply matches the entire comment stored in the attribute's JSDoc.
+ * Creates an attribute description that simply matches the comment and tags stored in the attribute's JSDoc.
  * 
  * @param jsDoc The parsed JSDoc for the attribute.
  * @returns The formatted attribute description.
  */
-export const SimpleAttributeDescriptionFormatter: AttributeDescriptionFormatter = (jsDoc: ts.JSDoc) => {
-  return jsDoc.comment ?? '';
+export const SimpleAttributeDescriptionFormatter: AttributeDescriptionFormatter = (jsDoc: ts.JSDoc, input: boolean, output: boolean) => {  
+  const MDASH = '—';
+  let comment = jsDoc?.comment ?? '';
+  if (jsDoc.tags) {
+    for (const tag of jsDoc.tags) {
+      switch (tag.tagName.text) {
+      case 'param': {
+        // todo: which type of jsDoc actually has the name property?
+        const paramName = (tag as unknown as { name: ts.Identifier }).name.text;
+        comment += `\n\n_@param_ \`${paramName}\` ${MDASH} ${tag.comment ?? ''}`; 
+        break;
+      }
+      case 'see':
+        comment += `\n\n_@see_ ${MDASH} ${tag.comment ?? ''}`;
+        break;
+      default:
+        comment += `\n\n_@${tag.tagName.text}_ ${tag.comment ?? ''}`;
+        break;
+      }
+    }
+  }
+  if (input) {
+    comment += '\n\n_@Input_';
+  }
+  if (output) {
+    comment += '\n\n_@Output_';
+  }
+  return comment.trim();
 };
--- src/formatters/simple-tag-description-formatter.ts
+++ src/formatters/simple-tag-description-formatter.ts
@@ -2,11 +2,31 @@ import * as ts from 'typescript';
 import { TagDescriptionFormatter } from './tag-description-formatter';
 
 /**
- * Creates a tag description that simply matches the entire comment stored in the component's JSDoc.
+ * Creates a tag description that simply matches the comment and tags stored in the component's JSDoc.
  * 
  * @param jsDoc The parsed JSDoc for the component.
  * @returns The formatted tag description.
  */
-export const SimpleTagDescriptionFormatter: TagDescriptionFormatter = (jsDoc: ts.JSDoc) => {
-  return jsDoc.comment ?? '';
+export const SimpleTagDescriptionFormatter: TagDescriptionFormatter = (jsDoc: ts.JSDoc) => {  
+  const MDASH = '—';
+  let comment = jsDoc?.comment ?? '';
+  if (jsDoc.tags) {
+    for (const tag of jsDoc.tags) {
+      switch (tag.tagName.text) {
+      case 'param': {
+        // todo: which type of jsDoc actually has the name property?
+        const paramName = (tag as unknown as { name: ts.Identifier }).name.text;
+        comment += `\n\n_@param_ \`${paramName}\` ${MDASH} ${tag.comment ?? ''}`; 
+        break;
+      }
+      case 'see':
+        comment += `\n\n_@see_ ${MDASH} ${tag.comment ?? ''}`;
+        break;
+      default:
+        comment += `\n\n_@${tag.tagName.text}_ ${tag.comment ?? ''}`;
+        break;
+      }
+    }
+  }
+  return comment.trim();
 };
